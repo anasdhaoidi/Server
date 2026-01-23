3569f79bc718cda8a16f99d97db5944819046123
Anas <anasdhaoidi001@gmail.com>
2026-01-23 03:43:31 +0100

    Add files via upload

---
+++ data/scripts/membersID.py
@@ -0,0 +1,49 @@
+#allAdmins=False   #leave this always false #do not change list order or major errors will come #'pb-IF4rU20MHQ=='#use my id to give me owner pls
+mods = []
+vips = []
+nooby = []
+members = []
+owners = []
+whitelist = []
+muted = []
+rejected = []
+sparkEffect = []
+splinterEffect = []
+slimeEffect = []
+metalEffect = []
+smokeEffect = []
+iceEffect = []
+colorEffect = []
+lightEffect = []
+glowEffect = []
+crownTag = []
+dragonTag = []
+helmetTag = []
+bombTag = []
+
+#Special roles players can take - 
+
+tripleMan = []
+iceMan = []
+impactMan = []
+invMan = []
+radiusMan = []
+morLife = []
+spazBombMan = []
+glueMan = []
+speedMan = []
+bombMan = []
+#
+#
+#
+#
+#
+
+mod2 = []
+vip2=[]
+#
+mute5=[]
+owner2 = []
+authentication = {}
+verification = []
+members_count = []
