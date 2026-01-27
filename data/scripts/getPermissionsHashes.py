3d7623759aa062cff373a127ebb46adc2197133e
Anas <anasdhaoidi001@gmail.com>
2026-01-27 14:07:54 +0100

    Add files via upload

---
+++ data/scripts/getPermissionsHashes.py
@@ -0,0 +1,25 @@
+adminHashes = []
+vipHashes = []
+banlist = {}
+topperslist = []
+effectCustomers = {}
+customlist = {'pb-IF4eFEgY': '\xee\x81\x89BelowAverage\xee\x81\x89'}
+ownerHashes = ['pb-IF4eFEgY','pb-IF4rU20MHQ==','pb-IF4BUlQBJw==']
+coownerHashes = []
+surroundingObjectEffect = []
+sparkEffect = []
+smokeEffect = []
+scorchEffect = [] 
+distortionEffect = []
+glowEffect = [] 
+iceEffect=[]
+slimeEffect = []
+metalEffect = []
+dragonHashes = []
+customtagHashes=[]
+whitelist = {}
+
+#donot change the order of the list
+#to enable/disable commands and effects for top 5 players goto settings.py
+#kindly keep vivek id for owner.
+
